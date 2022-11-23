20104c8aa60b2f16ec6c039cd568ec89a5129df2
amirversal <117270240+amirversal@users.noreply.github.com>
2022-11-23 11:59:57 +0100

    Add files via upload

---
+++ training/Training_Data04/load_set.m
@@ -0,0 +1,210 @@
+clear all
+
+load Capacity_beam_t
+load time
+load Payload_configuration_t
+
+load Trafic_beam_t.mat
+load Traffic_Matrix0001
+Traffic_Matrix0001 = Traffic_Matrix;
+load Traffic_Matrix0002
+Traffic_Matrix0002 = Traffic_Matrix;
+load Traffic_Matrix0003
+Traffic_Matrix0003 = Traffic_Matrix;
+load Traffic_Matrix0004
+Traffic_Matrix0004 = Traffic_Matrix;
+load Traffic_Matrix0005
+Traffic_Matrix0005 = Traffic_Matrix;
+load Traffic_Matrix0006
+Traffic_Matrix0006 = Traffic_Matrix;
+load Traffic_Matrix0007
+Traffic_Matrix0007 = Traffic_Matrix;
+load Traffic_Matrix0008
+Traffic_Matrix0008 = Traffic_Matrix;
+load Traffic_Matrix0009
+Traffic_Matrix0009 = Traffic_Matrix;
+load Traffic_Matrix0010
+Traffic_Matrix0010 = Traffic_Matrix;
+load Traffic_Matrix0011
+Traffic_Matrix0011 = Traffic_Matrix;
+load Traffic_Matrix0012
+Traffic_Matrix0012 = Traffic_Matrix;
+load Traffic_Matrix0013
+Traffic_Matrix0013 = Traffic_Matrix;
+load Traffic_Matrix0014
+Traffic_Matrix0014 = Traffic_Matrix;
+load Traffic_Matrix0015
+Traffic_Matrix0015 = Traffic_Matrix;
+load Traffic_Matrix0016
+Traffic_Matrix0016 = Traffic_Matrix;
+load Traffic_Matrix0017
+Traffic_Matrix0017 = Traffic_Matrix;
+load Traffic_Matrix0018
+Traffic_Matrix0018 = Traffic_Matrix;
+load Traffic_Matrix0019
+Traffic_Matrix0019 = Traffic_Matrix;
+load Traffic_Matrix0020
+Traffic_Matrix0020 = Traffic_Matrix;
+load Traffic_Matrix0021
+Traffic_Matrix0021 = Traffic_Matrix;
+load Traffic_Matrix0022
+Traffic_Matrix0022 = Traffic_Matrix;
+load Traffic_Matrix0023
+Traffic_Matrix0023 = Traffic_Matrix;
+load Traffic_Matrix0024
+Traffic_Matrix0024 = Traffic_Matrix;
+load Traffic_Matrix0025
+Traffic_Matrix0025 = Traffic_Matrix;
+load Traffic_Matrix0026
+Traffic_Matrix0026 = Traffic_Matrix;
+load Traffic_Matrix0027
+Traffic_Matrix0027 = Traffic_Matrix;
+load Traffic_Matrix0028
+Traffic_Matrix0028 = Traffic_Matrix;
+load Traffic_Matrix0029
+Traffic_Matrix0029 = Traffic_Matrix;
+load Traffic_Matrix0030
+Traffic_Matrix0030 = Traffic_Matrix;
+load Traffic_Matrix0031
+Traffic_Matrix0031 = Traffic_Matrix;
+load Traffic_Matrix0032
+Traffic_Matrix0032 = Traffic_Matrix;
+load Traffic_Matrix0033
+Traffic_Matrix0033 = Traffic_Matrix;
+load Traffic_Matrix0034
+Traffic_Matrix0034 = Traffic_Matrix;
+load Traffic_Matrix0035
+Traffic_Matrix0035 = Traffic_Matrix;
+load Traffic_Matrix0036
+Traffic_Matrix0036 = Traffic_Matrix;
+load Traffic_Matrix0037
+Traffic_Matrix0037 = Traffic_Matrix;
+load Traffic_Matrix0038
+Traffic_Matrix0038 = Traffic_Matrix;
+load Traffic_Matrix0039
+Traffic_Matrix0039 = Traffic_Matrix;
+load Traffic_Matrix0040
+Traffic_Matrix0040 = Traffic_Matrix;
+load Traffic_Matrix0041
+Traffic_Matrix0041 = Traffic_Matrix;
+load Traffic_Matrix0042
+Traffic_Matrix0042 = Traffic_Matrix;
+load Traffic_Matrix0043
+Traffic_Matrix0043 = Traffic_Matrix;
+load Traffic_Matrix0044
+Traffic_Matrix0044 = Traffic_Matrix;
+load Traffic_Matrix0045
+Traffic_Matrix0045 = Traffic_Matrix;
+load Traffic_Matrix0046
+Traffic_Matrix0046 = Traffic_Matrix;
+load Traffic_Matrix0047
+Traffic_Matrix0047 = Traffic_Matrix;
+load Traffic_Matrix0048
+Traffic_Matrix0048 = Traffic_Matrix;
+load Traffic_Matrix0049
+Traffic_Matrix0049 = Traffic_Matrix;
+load Traffic_Matrix0050
+Traffic_Matrix0050 = Traffic_Matrix;
+load Traffic_Matrix0051
+Traffic_Matrix0051 = Traffic_Matrix;
+load Traffic_Matrix0052
+Traffic_Matrix0052 = Traffic_Matrix;
+load Traffic_Matrix0053
+Traffic_Matrix0053 = Traffic_Matrix;
+load Traffic_Matrix0054
+Traffic_Matrix0054 = Traffic_Matrix;
+load Traffic_Matrix0055
+Traffic_Matrix0055 = Traffic_Matrix;
+load Traffic_Matrix0056
+Traffic_Matrix0056 = Traffic_Matrix;
+load Traffic_Matrix0057
+Traffic_Matrix0057 = Traffic_Matrix;
+load Traffic_Matrix0058
+Traffic_Matrix0058 = Traffic_Matrix;
+load Traffic_Matrix0059
+Traffic_Matrix0059 = Traffic_Matrix;
+load Traffic_Matrix0060
+Traffic_Matrix0060 = Traffic_Matrix;
+load Traffic_Matrix0061
+Traffic_Matrix0061 = Traffic_Matrix;
+load Traffic_Matrix0062
+Traffic_Matrix0062 = Traffic_Matrix;
+load Traffic_Matrix0063
+Traffic_Matrix0063 = Traffic_Matrix;
+load Traffic_Matrix0064
+Traffic_Matrix0064 = Traffic_Matrix;
+load Traffic_Matrix0065
+Traffic_Matrix0065 = Traffic_Matrix;
+load Traffic_Matrix0066
+Traffic_Matrix0066 = Traffic_Matrix;
+load Traffic_Matrix0067
+Traffic_Matrix0067 = Traffic_Matrix;
+load Traffic_Matrix0068
+Traffic_Matrix0068 = Traffic_Matrix;
+load Traffic_Matrix0069
+Traffic_Matrix0069 = Traffic_Matrix;
+load Traffic_Matrix0070
+Traffic_Matrix0070 = Traffic_Matrix;
+load Traffic_Matrix0071
+Traffic_Matrix0071 = Traffic_Matrix;
+load Traffic_Matrix0072
+Traffic_Matrix0072 = Traffic_Matrix;
+load Traffic_Matrix0073
+Traffic_Matrix0073 = Traffic_Matrix;
+load Traffic_Matrix0074
+Traffic_Matrix0074 = Traffic_Matrix;
+load Traffic_Matrix0075
+Traffic_Matrix0075 = Traffic_Matrix;
+load Traffic_Matrix0076
+Traffic_Matrix0076 = Traffic_Matrix;
+load Traffic_Matrix0077
+Traffic_Matrix0077 = Traffic_Matrix;
+load Traffic_Matrix0078
+Traffic_Matrix0078 = Traffic_Matrix;
+load Traffic_Matrix0079
+Traffic_Matrix0079 = Traffic_Matrix;
+load Traffic_Matrix0080
+Traffic_Matrix0080 = Traffic_Matrix;
+load Traffic_Matrix0081
+Traffic_Matrix0081 = Traffic_Matrix;
+load Traffic_Matrix0082
+Traffic_Matrix0082 = Traffic_Matrix;
+load Traffic_Matrix0083
+Traffic_Matrix0083 = Traffic_Matrix;
+load Traffic_Matrix0084
+Traffic_Matrix0084 = Traffic_Matrix;
+load Traffic_Matrix0085
+Traffic_Matrix0085 = Traffic_Matrix;
+load Traffic_Matrix0086
+Traffic_Matrix0086 = Traffic_Matrix;
+load Traffic_Matrix0087
+Traffic_Matrix0087 = Traffic_Matrix;
+load Traffic_Matrix0088
+Traffic_Matrix0088 = Traffic_Matrix;
+load Traffic_Matrix0089
+Traffic_Matrix0089 = Traffic_Matrix;
+load Traffic_Matrix0090
+Traffic_Matrix0090 = Traffic_Matrix;
+load Traffic_Matrix0091
+Traffic_Matrix0091 = Traffic_Matrix;
+load Traffic_Matrix0091
+Traffic_Matrix0092 = Traffic_Matrix;
+load Traffic_Matrix0093
+Traffic_Matrix0093 = Traffic_Matrix;
+load Traffic_Matrix0094
+Traffic_Matrix0094 = Traffic_Matrix;
+load Traffic_Matrix0095
+Traffic_Matrix0095 = Traffic_Matrix;
+load Traffic_Matrix0096
+Traffic_Matrix0096 = Traffic_Matrix;
+load Traffic_Matrix0097
+Traffic_Matrix0097 = Traffic_Matrix;
+load Traffic_Matrix0098
+Traffic_Matrix0098 = Traffic_Matrix;
+load Traffic_Matrix0099
+Traffic_Matrix0099 = Traffic_Matrix;
+load Traffic_Matrix0100
+Traffic_Matrix0100 = Traffic_Matrix;
+
+
+save('set4')
